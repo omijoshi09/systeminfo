0600ed3649c5f1e75eeeadf589e059b89d86d06f
omijo <omijoshi09@gmail.com>
2020-01-23 10:01:30 +0000

    New file updated

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-setup(name="System-info",
+setup(name="systeminfo",
       version="0.1",
       description="Basic system information for COMP30670",
       url="https://github.com/omijoshi09/systeminfo.git",
       author="Omkar Joshi",
       author_email="omkar.joshi@ucd.ie",
       licence="GPL3",
-      packages=['System-info'],
+      packages=['systeminfo'],
       entry_points={
-        'console_scripts':['comp30670_systeminfo=System-info.main:main']
+        'console_scripts':['comp30670_systeminfo=systeminfo.main:main']
         }
       )
